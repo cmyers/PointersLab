8e4acf8552b78ad8b3ed8f738e0023806d0302bb
Chris Myers <cmyers2015@outlook.com>
2018-06-13 22:02:17 +0100

    Added private and public members to Person class with implementations

---
+++ PointersLab/Person.cpp
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+#include "Person.h"
+
+Person::Person(int age, int height, int weight, std::string name) 
+{
+	_age - age;
+	_height = height;
+	_weight = weight;
+	_name = name;
+}
+
+int Person::getAge()
+{
+	return _age;
+}
+
+int Person::getHeight()
+{
+	return _height;
+}
+
+int Person::getWeight()
+{
+	return _weight;
+}
+
+std::string Person::getName()
+{
+	return _name;
+}
+
+void Person::setName(std::string name)
+{
+	_name = name;
+}
