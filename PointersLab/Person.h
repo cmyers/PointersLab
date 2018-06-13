250a280b69441f16db5e0e02974b13ceaab24687
Chris Myers <cmyers2015@outlook.com>
2018-06-13 22:02:17 +0100

    Added private and public members to Person class with implementations

--- PointersLab/Person.h
+++ PointersLab/Person.h
@@ -3,9 +3,17 @@
 
 class Person
 {
-	public:
+	private:
 		int _age;
 		int _height;
 		int _weight;
 		std::string _name;
+
+	public:
+		int getAge();
+		int getHeight();
+		int getWeight();
+		void setName(std::string name);
+		std::string getName();
+		Person(int age, int height, int weight, std::string name);
 };
